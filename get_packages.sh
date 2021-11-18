a8a6668bbc3a97c5e789ece099c6093a44b46331
Jose Gato <jgato@redhat.com>
2021-11-18 13:23:40 +0100

    updating scripts

--- get_packages.sh
+++ get_packages.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 # basic tools
-sudo dnf install -y vim jq wget tar bash-completion bind-util bind-utils
+sudo dnf install -y vim jq wget tar bash-completion bind-util bind-utils coreos-installer.x86_64
 
 # loadbalancer and pxe
 
-sudo dnf install -y haproxy.x86_64
+sudo dnf install -y haproxy.x86_64 tftp.x86_64
 
 # http server
 
