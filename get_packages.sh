f65ff1575605a4ce4c7bbc611e69c5b4d16c9d59
Jose Gato <jgato@redhat.com>
2021-11-19 16:46:30 +0100

    fixed some packages

--- get_packages.sh
+++ get_packages.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 # basic tools
-sudo dnf install -y vim jq wget tar bash-completion bind-util bind-utils coreos-installer.x86_64
+sudo dnf install -y vim jq wget tar bash-completion bind-utils coreos-installer.x86_64
 
 # loadbalancer and pxe
 
-sudo dnf install -y haproxy.x86_64 tftp.x86_64 tftp-server.x86_64 syslinux-tftpboot
+sudo dnf install -y haproxy.x86_64 tftp.x86_64 tftp-server.x86_64 syslinux-nonlinux.noarch
 
 # http server
 
